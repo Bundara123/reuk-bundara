832f5429a35091b19526646b05b471e4fe01952a
Bundara REUK <reuk.bundara@ababank.com>
2023-03-16 15:07:18 +0700

    fix issues

---
+++ _nuxt/about.4e8dbd97.js
@@ -0,0 +1,1 @@
+import{a as t}from"./entry.2d8ad519.js";const r={};function c(e,o){return" abouts "}const n=t(r,[["render",c]]);export{n as default};
---
+++ _nuxt/index.98e32ced.js
@@ -0,0 +1,1 @@
+import{r as e,t as a,u as t}from"./entry.2d8ad519.js";const _={__name:"index",setup(n){const r=e("reuk bundara");return(s,o)=>" home "+a(t(r))}};export{_ as default};
